28c32a6261ce93ecb7284ac33d7a94b9a5e249f0
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-07-16 02:26:59 +0600

    installed firebase

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,4 +1,9 @@
 const Login = () => {
+  // login handler
+  const handleLogin = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className='hero min-h-screen bg-base-200'>
       <div className='hero-content flex-col lg:flex-row-reverse'>
@@ -12,36 +17,45 @@ const Login = () => {
         </div>
         <div className='card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100'>
           <div className='card-body'>
-            <div className='form-control'>
-              <label className='label'>
-                <span className='label-text'>Email</span>
-              </label>
-              <input
-                name='email'
-                type='text'
-                placeholder='email'
-                className='input input-bordered'
-              />
-            </div>
-            <div className='form-control'>
-              <label className='label'>
-                <span className='label-text'>Password</span>
-              </label>
-              <input
-                name='password'
-                type='text'
-                placeholder='password'
-                className='input input-bordered'
-              />
-              <label className='label'>
-                <a href='#' className='label-text-alt link link-hover'>
-                  Forgot password?
-                </a>
-              </label>
-            </div>
-            <div className='form-control mt-6'>
-              <button className='btn btn-primary'>Login</button>
-            </div>
+            {/* login form */}
+            <form onSubmit={handleLogin}>
+              <div className='form-control'>
+                <label className='label'>
+                  <span className='label-text'>Email</span>
+                </label>
+                <input
+                  name='email'
+                  type='text'
+                  placeholder='email'
+                  className='input input-bordered'
+                />
+              </div>
+              <div className='form-control'>
+                <label className='label'>
+                  <span className='label-text'>Password</span>
+                </label>
+                <input
+                  name='password'
+                  type='text'
+                  placeholder='password'
+                  className='input input-bordered'
+                />
+                <label className='label'>
+                  <a href='#' className='label-text-alt link link-hover'>
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className='form-control mt-6'>
+                <input
+                  className='btn btn-primary'
+                  type='submit'
+                  value='Login'
+                />
+              </div>
+            </form>
+
+            {/* google login */}
             <div className='divider'>OR</div>
             <button className='btn btn-circle btn-outline mx-auto'>G</button>
             <p>Do not Have An Account? Please Sign Up</p>
