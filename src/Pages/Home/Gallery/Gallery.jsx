91c9b1ce00a255e69f2bc880b6b02c128bf6db4d
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-07-16 21:14:18 +0600

    gallery section

--- src/Pages/Home/Gallery/Gallery.jsx
+++ src/Pages/Home/Gallery/Gallery.jsx
@@ -1,11 +1,56 @@
-
-
 const Gallery = () => {
-    return (
-        <div>
-            <h2>this is gallery</h2>
+  return (
+    <>
+      <h2 className='text-5xl text-center font bold my-10'>Gallery Section</h2>
+      <div className='grid grid-cols-3'>
+        <div className='card w-96 glass'>
+          <figure>
+            <img
+              src='https://m.media-amazon.com/images/I/7139FuqxgjL.jpg'
+              alt='car!'
+            />
+          </figure>
+          <div className='card-body'>
+            <h2 className='card-title'>Astro Bot</h2>
+            <p>How to park your car at your garage?</p>
+            <div className='card-actions justify-end'>
+              <button className='btn btn-primary'>Learn now!</button>
+            </div>
+          </div>
         </div>
-    );
+        <div className='card w-96 glass'>
+          <figure>
+            <img
+              src='https://images.unsplash.com/photo-1546776310-eef45dd6d63c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=510&q=80'
+              alt='car!'
+            />
+          </figure>
+          <div className='card-body'>
+            <h2 className='card-title'>Mech Buddy</h2>
+            <p>How to park your car at your garage?</p>
+            <div className='card-actions justify-end'>
+              <button className='btn btn-primary'>Learn now!</button>
+            </div>
+          </div>
+        </div>
+        <div className='card w-96 glass'>
+          <figure>
+            <img
+              src='https://images.unsplash.com/photo-1549512329-31d4e8831543?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=435&q=80'
+              alt='car!'
+            />
+          </figure>
+          <div className='card-body'>
+            <h2 className='card-title'>Maxxi</h2>
+            <p>How to park your car at your garage?</p>
+            <div className='card-actions justify-end'>
+              <button className='btn btn-primary'>Learn now!</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Gallery;
