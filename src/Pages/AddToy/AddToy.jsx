b64f0dcde699de838b04ce5112ac93184b38ce52
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-07-17 18:28:37 +0600

    add a toy form created

--- src/Pages/AddToy/AddToy.jsx
+++ src/Pages/AddToy/AddToy.jsx
@@ -1,11 +1,154 @@
-
+import { useContext } from "react";
+import Footer from "../../Shared/Footer/Footer";
+import Navbar from "../../Shared/Navbar/Navbar";
+import { AuthContext } from "../../Providers/AuthProvider";
 
 const AddToy = () => {
-    return (
-        <div>
-            <h2>Add A Toy Page</h2>
+  const { user } = useContext(AuthContext);
+
+  const handleAddToy = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+    const picture = form.picture.value;
+    const toy_name = form.toy_name.value;
+    const seller_name = form.seller_name.value;
+    const seller_email = user?.email;
+    const sub_category = form.sub_category.value;
+    const price = form.price.value;
+    const available_quantity = form.available_quantity.value;
+    const detail = form.detail.value;
+
+    const info = {
+      picture,
+      toy_name,
+      seller_name,
+      seller_email,
+      sub_category,
+      price,
+      available_quantity,
+      detail,
+    };
+
+    console.log(info);
+  };
+
+  return (
+    <div>
+      <Navbar></Navbar>
+      <h2 className='text-3xl font-bold text-center'>Add Your Toy</h2>
+      <form onSubmit={handleAddToy}>
+        <div className='hero min-h-screen bg-base-200'>
+          <div className='hero-content flex-col lg:flex-row-reverse'>
+            <div className='card  w-full shadow-2xl bg-base-100'>
+              <div className='card-body grid grid-cols-2'>
+                <div className='form-control'>
+                  <label className='label'>
+                    <span className='label-text'>Picture </span>
+                  </label>
+                  <input
+                    name='picture'
+                    type='text'
+                    placeholder=''
+                    className='input input-bordered'
+                  />
+                </div>
+                <div className='form-control'>
+                  <label className='label'>
+                    <span className='label-text'>Toy Name</span>
+                  </label>
+                  <input
+                    name='toy_name'
+                    type='text'
+                    placeholder=''
+                    className='input input-bordered'
+                  />
+                </div>
+                <div className='form-control '>
+                  <label className='label'>
+                    <span className='label-text'>Seller Name </span>
+                  </label>
+                  <input
+                    name='seller_name'
+                    type='text'
+                    placeholder=''
+                    defaultValue={user?.displayName}
+                    className='input input-bordered'
+                    readOnly
+                  />
+                </div>
+                <div className='form-control'>
+                  <label className='label'>
+                    <span className='label-text'>Seller Email</span>
+                  </label>
+                  <input
+                    name='seller_email'
+                    type='email'
+                    defaultValue={user?.email}
+                    placeholder=''
+                    className='input input-bordered'
+                    readOnly
+                  />
+                </div>
+                <div className='form-control '>
+                  <label className='label'>
+                    <span className='label-text'>Sub-Category</span>
+                  </label>
+                  <input
+                    name='sub_category'
+                    type='text'
+                    placeholder=''
+                    className='input input-bordered'
+                  />
+                </div>
+                <div className='form-control'>
+                  <label className='label'>
+                    <span className='label-text'>Price</span>
+                  </label>
+                  <input
+                    name='price'
+                    type='text'
+                    placeholder=''
+                    className='input input-bordered'
+                  />
+                </div>
+                <div className='form-control '>
+                  <label className='label'>
+                    <span className='label-text'>Available Quantity</span>
+                  </label>
+                  <input
+                    name='available_quantity'
+                    type='text'
+                    placeholder=''
+                    className='input input-bordered'
+                  />
+                </div>
+                <div className='form-control'>
+                  <label className='label'>
+                    <span className='label-text'>Detail</span>
+                  </label>
+                  <input
+                    name='detail'
+                    type='text'
+                    placeholder=''
+                    className='input input-bordered'
+                  />
+                </div>
+                <div className='form-control mt-6'>
+                  <input
+                    className='btn btn-primary'
+                    type='submit'
+                    value='Add Toy'
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </form>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default AddToy;
